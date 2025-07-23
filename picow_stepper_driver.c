5685f7e4ec7a0e04d054f02ffa1f5a90528362de
alan36alexis <alanvalexis@gmail.com>
2025-07-23 08:26:58 -0300

    docs: add comments

--- picow_stepper_driver.c
+++ picow_stepper_driver.c
@@ -1,33 +1,33 @@
 #include "pico/stdlib.h"
 #include "tmc2208/tmc2208.h"
 
-// --- Configuración ---
-// Define los pines GPIO que vas a usar
+// --- Configuracion ---
+// Deficino de pines
 #define MOTOR_STEP_PIN 14
 #define MOTOR_DIR_PIN  15
 #define MOTOR_ENA_PIN  16
 
-// Define los parámetros de tu motor
+// Parámetros de resolucion
 #define MOTOR_STEPS_PER_REV 200
 #define MOTOR_MICROSTEPS    32
 
 int main() {
     stdio_init_all();
 
-    // Crear una instancia de la estructura del motor
     TMC2208_t motor1;
 
-    // Inicializar el motor con los pines y parámetros definidos
     tmc2208_init(&motor1, MOTOR_STEP_PIN, MOTOR_DIR_PIN, MOTOR_ENA_PIN, MOTOR_STEPS_PER_REV, MOTOR_MICROSTEPS);
-    sleep_ms(1000); // Espera para asegurar que el motor esté listo
-    // Habilitar el motor
-    tmc2208_enable(&motor1, true);
+    sleep_ms(200);
 
+    tmc2208_enable(&motor1, true);
     // tmc2208_set_direction(&motor1, true);
     // tmc2208_set_rpm(&motor1, 60);
 
     while (1) {
-        // Girar en una dirección a 60 RPM por 5 segundos
+
+        //enviar 500 pasos a 1000 Hz y esperar 1 segundo
+        //reaccion: durante 500 ms hay pulsos en step_pin y durante 500 ms no hay pulsos
+
         tmc2208_set_direction(&motor1, true);
         tmc2208_send_nsteps_at_freq(&motor1, 10, 20.0f); // Enviar 500 pasos a 1000 Hz
 
